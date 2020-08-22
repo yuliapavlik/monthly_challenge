fc0cd5fdc96eeae7a7df362d4aea98cfb76f6717
yurakekc <acxz120@gmail.com>
2020-08-22 17:48:42 +0300

    Add back-end listener

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Check.css';
 import Header from './Header'
 import Check from "./Check";
@@ -6,8 +6,25 @@ import background from "./background2.jpg";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import {BrowserRouter, Route} from "react-router-dom";
- const App = () => {
 
+class App extends Component {
+
+	constructor(props) {
+	    super(props);
+	    this.state = { apiResponse: "" };
+	}
+
+	callAPI() {
+	    fetch("http://localhost:9000/testAPI")
+		.then(res => res.text())
+		.then(res => this.setState({ apiResponse: res }));
+	}
+
+	componentWillMount() {
+	    this.callAPI();
+	}
+
+	render() {
         return (
             <BrowserRouter>
             <div>
@@ -44,14 +61,14 @@ import {BrowserRouter, Route} from "react-router-dom";
                     <Check/>
                     <Check/>
 
-                </div>
+               </div>
                <Route component={Signin} />
-                <Route component={Signup} />
-            </div>
-                </BrowserRouter>
+               <Route component={Signup} />
+               </div>
+	       <p className="App-intro">;{this.state.apiResponse}</p>
+               </BrowserRouter>
         )
     }
+}
 
-
-
-export default App
+export default App;
